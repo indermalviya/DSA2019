c688d0950878f25950d7d9e1d6a0080e10dc73a2
Atul Sharma <atulsharma21772@gmail.com>
2019-07-28 18:27:56 +0530

    more traversal...
    Signed-off-by: Atul Sharma <atulsharma21772@gmail.com>

--- src/com/DSA2019/Tree/BinarytreeTraversals.java
+++ src/com/DSA2019/Tree/BinarytreeTraversals.java
@@ -3,6 +3,9 @@
  */
 package com.DSA2019.Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author Atul Sharma
  *
@@ -22,7 +25,7 @@ public class BinarytreeTraversals {
 		btt.root.left.right = new BinaryTreeNode(5);
 		btt.root.right.left = new BinaryTreeNode(6);
 		btt.root.right.right = new BinaryTreeNode(7);
-		
+
 		System.out.println("PreOrder tree traversal ");
 		btt.preOrderTraversal(btt.root);
 		System.out.println();
@@ -31,30 +34,55 @@ public class BinarytreeTraversals {
 		System.out.println();
 		System.out.println("InOrder tree traversal ");
 		btt.inOrderTraversal(btt.root);
-		
+		System.out.println();
+		System.out.println("LevelOrder tree traversal ");
+		btt.levelOrderTraversal(btt.root);
 
+	}
+
+	/**
+	 * @param root
+	 */
+	private void levelOrderTraversal(BinaryTreeNode root) {
+		Queue<BinaryTreeNode> queue = new LinkedList<BinaryTreeNode>();
+		if (root != null) {
+			queue.add(root);
+		} else {
+			return;
+		}
+
+		while (!queue.isEmpty()) {
+			BinaryTreeNode temp = queue.poll();
+			System.out.print(temp.data + " ");
+			if (temp.left != null) {
+				queue.add(temp.left);
+			}
+			if (temp.right != null) {
+				queue.add(temp.right);
+			}
+		}
 	}
 
 	/**
 	 * @param root
 	 */
 	private void inOrderTraversal(BinaryTreeNode root) {
-		if(root!=null) {
+		if (root != null) {
 			inOrderTraversal(root.left);
-			System.out.print(root.data+ " ");
+			System.out.print(root.data + " ");
 			inOrderTraversal(root.right);
 		}
-		
+
 	}
 
 	/**
 	 * @param root
 	 */
 	private void postOrderTraversal(BinaryTreeNode root) {
-		if(root!=null) {
+		if (root != null) {
 			postOrderTraversal(root.left);
 			postOrderTraversal(root.right);
-			System.out.print(root.data+ " ");
+			System.out.print(root.data + " ");
 		}
 	}
 
@@ -62,8 +90,8 @@ public class BinarytreeTraversals {
 	 * @param root
 	 */
 	private void preOrderTraversal(BinaryTreeNode root) {
-		if(root!=null) {
-			System.out.print(root.data+ " ");
+		if (root != null) {
+			System.out.print(root.data + " ");
 			preOrderTraversal(root.left);
 			preOrderTraversal(root.right);
 		}
